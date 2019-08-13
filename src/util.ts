1fb7cc49538c84e3227805d5080701502154ff2b
lukaskurz <lukaskurz5@gmail.com>
2019-08-13 15:26:46 +0200

    added margin of error calculation

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export function median(array: number[]) {
     let sorted = array.sort();
     if (sorted.length % 2 === 0) { // array with even number elements
@@ -6,4 +8,22 @@ export function median(array: number[]) {
     else {
         return sorted[(sorted.length - 1) / 2]; // array with odd number elements
     }
+}
+
+const zScores = {
+    "80": 1.28,
+    "85": 1.44,
+    "90": 1.65,
+    "95": 1.96,
+    "99": 2.58,
+}
+
+export function marginOfError(values: number[], confidenceLevel: 80 | 85 | 90 | 95 | 99 = 95) {
+    const mean = _.mean(values);
+    const sqrErrSum = _.sumBy(values, (t) => (t - mean) ** 2);
+    const avgSqrErr = sqrErrSum / values.length;
+    const popStdDev = Math.sqrt(avgSqrErr);
+    const moe = (popStdDev / Math.sqrt(values.length)) * zScores[confidenceLevel];
+
+    return moe;
 }

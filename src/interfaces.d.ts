eb11f1f2dca08535a566c6c81f52f9f33f18e2b3
lukaskurz <lukaskurz5@gmail.com>
2019-08-20 15:58:49 +0200

    added typings for global functions

---
+++ src/interfaces.d.ts
@@ -0,0 +1,6 @@
+declare function structure(name: string, fn: Function): void;
+declare function before(fn: Function): void;
+declare function after(fn: Function): void;
+declare function beforeEach(fn: Function): void;
+declare function afterEach(fn: Function): void;
+declare function benchmark(name: string, fn: Function): void;

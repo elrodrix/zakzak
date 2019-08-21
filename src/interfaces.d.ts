ab9bf189074838c0858fbb79eb4924dce2ef2598
lukaskurz <lukaskurz5@gmail.com>
2019-08-21 11:25:01 +0200

    deleted unnecessary typings file

--- src/interfaces.d.ts
+++
@@ -1,6 +0,0 @@
-declare function structure(name: string, fn: Function): void;
-declare function before(fn: Function): void;
-declare function after(fn: Function): void;
-declare function beforeEach(fn: Function): void;
-declare function afterEach(fn: Function): void;
-declare function benchmark(name: string, fn: Function): void;

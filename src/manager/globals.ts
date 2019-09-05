9017aed54e64afd5e6814e83000b340cb1ab10db
Lukas Kurz <lukaskurz5@gmail.com>
2019-09-05 13:23:08 +0200

    redefined globals

--- src/manager/globals.ts
+++ src/manager/globals.ts
@@ -1,7 +1,6 @@
 import BenchmarkManager from "./benchmark-manager";
-import Structure from "./structure";
-import Benchmark from "./../benchmark/benchmark";
 import { BenchmarkOptions } from "../config/options";
+import OptionsManager from "@zakzak/config/options-manager";
 
 export { };
 
@@ -9,34 +8,27 @@ declare global {
 	/**
 	 * Used to define an enclosing structure inside a benchmark file. Multiple structures can be neighbours and/or nested
 	 * @param name Name of the structure
-	 * @param callback Structure or benchmark inside this structure
+	 * @param fn Structure or benchmark inside this structure
 	 * @param options Options that will be applied to all  benchmarks enclosed in this structure
 	 */
-	function structure(name: string, callback: Function, options?: BenchmarkOptions): void;
+	function structure(name: string, fn: Function, options?: BenchmarkOptions): void;
 
 	/**
 	 * Used to define an benchmark
 	 * @param name Name of the benchmark
-	 * @param callback Function that will be benchmarked
+	 * @param fn Function that will be benchmarked
 	 * @param options Options that will be applied for this specific benchmark
 	 */
-	function benchmark(name: string, callback: Function, options?: BenchmarkOptions): void;
+	function benchmark(name: string, fn: Function, options?: BenchmarkOptions): void;
 }
 
 // tslint:disable-next-line: variable-name
 const _global = global as any;
-_global.structure = (name: string, callback: Function, options?: BenchmarkOptions) => {
-	const s = new Structure(name, callback, undefined, options);
+_global.structure = (name: string, fn: Function, options: BenchmarkOptions = OptionsManager.benchmarkOptions) => {
 	const mng = BenchmarkManager.getInstance();
-	mng.addStructure(s);
-	return s;
+	mng.addStructure(name, fn, options);
 };
-_global.benchmark = (name: string, callback: Function, options?: BenchmarkOptions) => {
-	const b = new Benchmark(name, callback, undefined, options);
+_global.benchmark = (name: string, fn: Function, options: BenchmarkOptions = OptionsManager.benchmarkOptions) => {
 	const mng = BenchmarkManager.getInstance();
-	mng.addBenchmark(b);
-	return b;
+	mng.addBenchmark(name, fn, options);
 };
-
-// type StructureFunction = (name: string, callback: StructureFunction | BenchmarkFunction, options?: BenchmarkOptions) => void;
-// type BenchmarkFunction = (name: string, callback: Function, options?: BenchmarkOptions) => void;

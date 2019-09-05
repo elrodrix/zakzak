26a5238a770d91ff464b4af85e7b159265c45c10
Lukas Kurz <lukaskurz5@gmail.com>
2019-09-05 09:16:08 +0200

    created timer

---
+++ src/benchmark/timer.ts
@@ -0,0 +1,21 @@
+import TimeUnit from "@timeunit";
+import _ from "lodash";
+
+export default class Timer {
+	public static getTime(unit: TimeUnit = TimeUnit.Nanosecond) {
+		const time = process.hrtime();
+		return (time[0] * TimeUnit.Second + time[1]) / unit;
+	}
+
+	public static getResolution() {
+		let count = 50;
+		const times: number[] = [];
+		while (count--) {
+			let now;
+			const begin = Timer.getTime();
+			while (!((now = Timer.getTime()) - begin)) { }
+			times.push(now - begin);
+		}
+		return _.mean(times);
+	}
+}

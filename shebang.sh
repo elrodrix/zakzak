2f55b92e6fb622fc42f590abac29670e658b2998
Lukas Kurz <lukaskurz5@gmail.com>
2019-09-20 15:42:04 +0200

    removed shebang helper stuff

--- shebang.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'prepending shebang to ./dist/cli-script.js'
-
-echo '0a
-#!/bin/sh
-":" //# comment; exec /usr/bin/env node --allow-natives-syntax "$0" "$@"
-.
-w' | ed ./dist/cli-script.js
--- src/cli-script.ts
+++ src/cli-script.ts
@@ -1,10 +1,5 @@
+#!/usr/bin/env node
 
-/*
-shebang.js will insert a shebang somewhere above this comment
-shebang hack: http://sambal.org/2014/02/passing-options-node-shebang-line/
-shebang has to be inserted after transpilation, otherwise tsc will insert semicolons between ":" and //
-which would make it no longer work.
-*/
 import path from "path";
 import globby from "globby";
 import { BenchmarkManager } from "./manager";

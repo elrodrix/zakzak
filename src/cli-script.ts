78d307e61108a065cb358f0c06ff691951560aac
Lukas Kurz <lukaskurz5@gmail.com>
2019-10-22 11:31:29 +0200

    remove commented out code

--- src/cli-script.ts
+++ src/cli-script.ts
@@ -16,38 +16,6 @@
  * limitations under the License.
  */
 
-// import path from "path";
-// import globby from "globby";
-// import BenchmarkManager from "./manager";
-// import CLIManager from "./cli";
-// import { OptionsManager } from "./config";
-// import { SuiteManager } from "./suite";
-// import { ExportManager } from "./exporter";
-
-// CLIManager.init();
-// const paramOptions = CLIManager.getOptions();
-
-// const options = new OptionsManager();
-// options.change(paramOptions);
-
-// const pattern = path.posix.join(
-//   options.benchmarkManagerOptions.path,
-//   options.benchmarkManagerOptions.pattern,
-// );
-// const files = globby.sync(pattern, { absolute: true });
-
-// CLIManager.printHeader();
-
-// const exporter = new ExportManager(options.benchmarkManagerOptions);
-
-// const suite = new SuiteManager(options.benchmarkOptions);
-// suite.addFiles(files);
-
-// exporter.exportHierarchy(suite.files);
-
-// const manager = new BenchmarkManager(suite.benchmarks, options.benchmarkManagerOptions, exporter);
-// manager.run();
-
 import path from "path";
 import { CLIManager } from ".";
 
